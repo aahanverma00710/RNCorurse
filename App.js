a780f9328270f645d6acd387463403781e8213b5
Aahan <aahanverma0123@gmail.com>
2026-03-02 23:18:57 +0530

    Components added

--- App.js
+++ App.js
@@ -1,34 +1,83 @@
 
-import { StyleSheet, Text, View, Button ,TextInput} from 'react-native';
+import { StyleSheet, Text, View, Button ,TextInput, ScrollView,FlatList} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goadInputHandler(enteredText) {
+     setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+      if(enteredGoalText.trim().length === 0) {
+        console.warn('Please enter a valid goal');
+        return;
+      }
+      setCourseGoals((currentCourseGoals) => [...currentCourseGoals, {text: enteredGoalText, id: Math.random().toString()}]);
+  }
+
+
   return (
     <View style={styles.appConinter} >
        <View style={styles.inputContainer} >
-          <TextInput style={styles.textInput} placeholder="Your course gole"/>
-          <Button title="Add Goal" />
+          <TextInput style={styles.textInput} placeholder="Your course gole"
+          onChangeText={goadInputHandler}/>
+          <Button title="Add Goal" onPress={addGoalHandler}/>
        </View>
-       <View>
-          <Text>List of goals</Text>
+       <View style={styles.goalsContainer} >
+         <FlatList data={courseGoals} 
+         renderItem={(itemData) => {
+          return (
+            <View  style={styles.goalItem} >
+              <Text style={styles.goalText}>{itemData.item.text}</Text>
+            </View>
+          )
+         }
+        } keyExtractor={(item, index) => {return item.id}}/>
        </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
     appConinter: {
-      padding: 50,
+      flex: 1,
+      paddingTop: 50,
+      paddingHorizontal: 16,
     },
     inputContainer: {
+      flex: 1,
       flexDirection: 'row',
       justifyContent: 'space-between',
+      alignItems: 'center',
+      marginBottom: 24,
+      borderBottomWidth: 1,
+      borderBottomColor: '#cccccc',
     },
     textInput: {
       borderBottomColor: '#cccccc',
       borderWidth: 1,
-      width: '80%',
+      width: '70%',
       marginRight: 8,
       padding: 8,
+    },
+    
+    goalsContainer: {
+      flex: 8,
+    },
+    goalItem: {
+      marginBottom: 8,
+      padding: 8,
+      backgroundColor: '#5e08cc',
+      borderRadius: 4,
+  
+    },
+    goalText: {
+      color: '#ffffff',
     },
   
 });
