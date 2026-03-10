5a15718c6de2f90127e62303ae841ce8b3dcfe03
Aahan <aahanverma0123@gmail.com>
2026-03-11 00:16:29 +0530

    Game Course

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,44 @@
+import {TextInput, View, StyleSheet} from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+
+function StartGameScreen() {
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput style={styles.numberInput} maxLength={2}  keyboardType='number-pad'
+            autoCapitalize='none' autoCorrect={false}/>
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+    );
+}
+
+export default StartGameScreen;
+
+
+const styles = StyleSheet.create({
+    inputContainer: {
+       marginHorizontal: 24,
+       padding: 16,
+       marginTop: 100,
+       borderRadius: 8,
+       backgroundColor: '#4e0329',
+       elevation: 4,
+       shadowColor: 'black',
+       shadowOffset: {width: 0, height: 2},
+       shadowRadius: 6,
+       shadowOpacity: 0.25,
+    },
+    numberInput :{
+        width : 50,
+        height: 55,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 1,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+          textAlign: 'center',
+    },
+   
+})
